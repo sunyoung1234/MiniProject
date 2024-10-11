5f7cc8e06dbc0f60db8a18a225d5315b8cc01579
302-1 <302-1@302-16>
2024-10-11 16:03:43 +0900

    memberController 추가 - 팀장

---
+++ MiniProject/src/main/java/member/web/MemberController.java
@@ -0,0 +1,15 @@
+package member.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping("/loginView")
+	public String loginView() {
+		
+		return "home";
+	}
+	
+}
