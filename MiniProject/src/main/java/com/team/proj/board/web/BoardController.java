0c212da56835b930a7846e875d7aef72a212bdad
ZENolepsy <usrn11@naver.com>
2024-10-15 14:20:15 +0900

    Merge branch 'main' of https://github.com/sunyoung1234/MiniProject.git into main

--- MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
+++ MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
@@ -9,27 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.team.proj.materials.dto.MaterialsDTO;
 import com.team.proj.materials.service.MaterialsService;
+import com.team.proj.board.dto.BoardDTO;
+import com.team.proj.board.service.BoardService;
+import com.team.proj.board.vo.SearchVO;
 
 @Controller
 public class BoardController {
-	
-	@Autowired
-	MaterialsService matService;
-	
-	@RequestMapping("/boardView")
-	public String boardView() {
-		
-		return "board/boardView";
-	}
-	
-	@RequestMapping("/boardWriteView")
-	public String boardWriteView(Model model) {
-		
-		List<MaterialsDTO> matList = matService.getMatList();
-		model.addAttribute("keyMatList", matList);
-		
-		
-		return "board/boardWriteView";
-		
-	}
+   
+    @Autowired
+    MaterialsService matService;
+
+    @Autowired
+    BoardService boardService;
+
+    @RequestMapping("/boardView")
+    public String boardView(Model model, SearchVO search) {
+        List<BoardDTO> boardList = boardService.getBoardList(search);
+        model.addAttribute("boardList", boardList);
+        return "board/boardView";
+    }
+
+    @RequestMapping("/boardWriteView")
+    public String boardWriteView(Model model) {
+        List<MaterialsDTO> matList = matService.getMatList();
+        model.addAttribute("keyMatList", matList);
+        return "board/boardWriteView";
+    }
 }
