30f108828e702965bd89bcc66b720abe8fbfe7cd
302-1 <302-1@302-17>
2024-10-15 17:39:36 +0900

    Merge branch 'main' of https://github.com/sunyoung1234/MiniProject.git into main

--- MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
+++ MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
@@ -5,34 +5,44 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.team.proj.board.dto.BoardDTO;
+import com.team.proj.board.service.BoardService;
+import com.team.proj.board.vo.SearchVO;
 import com.team.proj.materials.dto.MaterialsDTO;
 import com.team.proj.materials.service.MaterialsService;
 
 @Controller
 public class BoardController {
-	
-	@Autowired
-	MaterialsService matService;
-	
-	@RequestMapping("/boardView")
-	public String boardView() {
-		
-		return "board/boardView";
-	}
-	
-	@RequestMapping("/boardWriteView")
-	public String boardWriteView(Model model) {
-		
-		List<MaterialsDTO> matList = matService.getMatList();
-		model.addAttribute("keyMatList", matList);
-		
-		
-		return "board/boardWriteView";
-		
-	}
-	
-	
+   
+    @Autowired
+    MaterialsService matService;
+
+    @Autowired
+    BoardService boardService;
+
+    @RequestMapping("/boardView")
+    public String boardView(Model model, SearchVO search) {
+        List<BoardDTO> boardList = boardService.getBoardList(search);
+        model.addAttribute("boardList", boardList);
+        return "board/boardView"	;
+    }
+
+    @RequestMapping("/boardWriteView")
+    public String boardWriteView(Model model) {
+        List<MaterialsDTO> matList = matService.getMatList();
+        model.addAttribute("keyMatList", matList);
+        return "board/boardWriteView";
+    }
+    
+    // 게시글 상세보기 메소드
+    @RequestMapping("/boardDetailView/{boardNo}")
+    public String boardDetailView(@PathVariable int boardNo, Model model) {
+        // 게시글 정보를 가져옴
+        BoardDTO boardDetail = boardService.getBoard(boardNo);
+        model.addAttribute("board", boardDetail);
+        return "board/boardDetailView"; // JSP 파일 경로
+    }
 }
