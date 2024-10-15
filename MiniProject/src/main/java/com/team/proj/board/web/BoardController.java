243da6843c3687fead5c49be7525e9f6206ed210
302-1 <302-1@302-17>
2024-10-15 17:34:49 +0900

    계산기 작업

--- MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
+++ MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.team.proj.materials.dto.MaterialsDTO;
 import com.team.proj.materials.service.MaterialsService;
@@ -32,4 +33,6 @@ public class BoardController {
 		return "board/boardWriteView";
 		
 	}
+	
+	
 }
