a08698b626a0513f9339a328a17a1a27e9476a42
302-1 <302-1@302-17>
2024-10-18 17:54:31 +0900

    mkt

--- MiniProject/src/main/java/com/team/proj/reply/web/ReplyController.java
+++ MiniProject/src/main/java/com/team/proj/reply/web/ReplyController.java
@@ -1,5 +1,8 @@
 package com.team.proj.reply.web;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ReplyController {
 
 }
--- MiniProject/src/main/java/com/team/proj/savecal/dao/ISavecalDAO.java
+++ MiniProject/src/main/java/com/team/proj/savecal/dao/ISavecalDAO.java
@@ -5,15 +5,23 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.team.proj.savecal.dto.SavecalDTO;
+import com.team.proj.savecal.dto.SavecalVolDTO;
 
 @Mapper
 public interface ISavecalDAO {
 	
+	// savecal 추가하기
 	int addSavecal(SavecalDTO sc);
 	
+	// calcId 로 찾기
 	List<SavecalDTO> findById(String calcId);
 	
+	// calcId confirm_yn = Y 로 업데이트
 	int updateConfirm(String calcId);
 	
+	// calcId confirm_yn = N 인것 들 삭제
 	int deleteConfirmN();
+	
+	// inner Join 으로 materials의 name , img 추가 SavecalVolDTO
+	List<SavecalVolDTO> getScVol(String calcId);
 }
