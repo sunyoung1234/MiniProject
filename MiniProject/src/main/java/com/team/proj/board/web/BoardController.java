e32fd4a0bc28ed1ed50b1fae955a185f4c42f30a
302-1 <302-1@302-17>
2024-10-21 10:20:21 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/sunyoung1234/MiniProject.git
    into mktmkt

--- MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
+++ MiniProject/src/main/java/com/team/proj/board/web/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.team.proj.board.dto.BoardDTO;
 import com.team.proj.board.service.BoardService;
@@ -19,110 +20,110 @@ import com.team.proj.board.vo.SearchVO;
 import com.team.proj.materials.dto.MaterialsDTO;
 import com.team.proj.materials.service.MaterialsService;
 import com.team.proj.member.dto.MemberDTO;
+import com.team.proj.reply.dto.ReplyDTO;
+import com.team.proj.reply.service.ReplyService;
 import com.team.proj.savecal.dto.SavecalDTO;
 import com.team.proj.savecal.dto.SavecalVolDTO;
 import com.team.proj.savecal.service.SavecalService;
 
 @Controller
 public class BoardController {
-   
-    @Autowired
-    MaterialsService matService;
 
-    @Autowired
-    BoardService boardService;
-    
-    @Autowired
-    SavecalService scService;
+	@Autowired
+	MaterialsService matService;
 
-    @RequestMapping("/boardView")
-    public String boardView(Model model, SearchVO search, HttpSession session) {
-        MemberDTO login = (MemberDTO) session.getAttribute("login");
-        String memId = login.getMemId();
-        
-        List<BoardDTO> boardListById = boardService.getBoardListById(memId);
-        
-        if(boardListById.size()>0) {
-        	model.addAttribute("boardListById", boardListById);
-        }else {
-        	model.addAttribute("noList","작성한 글이 없습니다.");
-        }
-        
-        System.out.println(boardListById);
-        
-        return "board/boardView"	;
-    }
+	@Autowired
+	BoardService boardService;
 
-    @RequestMapping("/boardWriteView")
-    public String boardWriteView(Model model, HttpSession session) {
-        List<MaterialsDTO> matList = matService.getMatList();
-        MemberDTO mem = (MemberDTO) session.getAttribute("login");
-       
-        
-        model.addAttribute("keyMatList", matList);
-        return "board/boardWriteView";
-    }
-    
-    // 게시글 상세보기 메소드
-    @RequestMapping("/boardDetailView")
-    public String boardDetailView(int orderNo, Model model, HttpSession session) {
-    	
-    	 List<MaterialsDTO> matList = matService.getMatList();
-         MemberDTO mem = (MemberDTO) session.getAttribute("login");
-        
-         
-         model.addAttribute("keyMatList", matList);
-    	
-        // 게시글 정보를 가져옴
-    	System.out.println(orderNo);
-        BoardDTO boardDetail = boardService.getBoard(orderNo);
-        model.addAttribute("board", boardDetail);
-        
-        String calcId = boardDetail.getCalcId();
-        double calcResult = boardDetail.getCalcResult();
-        
-        List<SavecalVolDTO> scVolList = scService.getScVol(calcId);
-        
-        model.addAttribute("scVolList", scVolList);
-        model.addAttribute("calcResult", calcResult);
+	@Autowired
+	SavecalService scService;
+	
+	@Autowired
+	ReplyService replyService;
 
-        System.out.println(scVolList);
-        return "board/boardDetailView"; // JSP 파일 경로
-    }
-    
-    
+	@RequestMapping("/boardView")
+	public String boardView(Model model, SearchVO search, HttpSession session) {
+		MemberDTO login = (MemberDTO) session.getAttribute("login");
+		String memId = login.getMemId();
+
+		List<BoardDTO> boardListById = boardService.getBoardListById(memId);
+
+		if (boardListById.size() > 0) {
+			model.addAttribute("boardListById", boardListById);
+		} else {
+			model.addAttribute("noList", "작성한 글이 없습니다.");
+		}
+
+		System.out.println(boardListById);
+
+		return "board/boardView";
+	}
+
+	@RequestMapping("/boardWriteView")
+	public String boardWriteView(Model model, HttpSession session) {
+		List<MaterialsDTO> matList = matService.getMatList();
+		MemberDTO mem = (MemberDTO) session.getAttribute("login");
+
+		model.addAttribute("keyMatList", matList);
+		return "board/boardWriteView";
+	}
+
+	// 게시글 상세보기 메소드
+	@RequestMapping("/boardDetailView")
+	public String boardDetailView(int orderNo, Model model, HttpSession session) {
+
+		List<MaterialsDTO> matList = matService.getMatList();
+		MemberDTO mem = (MemberDTO) session.getAttribute("login");
+
+		model.addAttribute("keyMatList", matList);
+
+		// 게시글 정보를 가져옴
+		System.out.println(orderNo);
+		BoardDTO boardDetail = boardService.getBoard(orderNo);
+		model.addAttribute("board", boardDetail);
+
+		String calcId = boardDetail.getCalcId();
+		double calcResult = boardDetail.getCalcResult();
+
+		List<SavecalVolDTO> scVolList = scService.getScVol(calcId);
+
+		model.addAttribute("scVolList", scVolList);
+		model.addAttribute("calcResult", calcResult);
+
+		System.out.println(scVolList);
+		return "board/boardDetailView"; // JSP 파일 경로
+	}
+
 	@RequestMapping("/boardWriteDo")
-	public String practice(@RequestBody Map<String,String> data,HttpSession session) {
-		
+	public String practice(@RequestBody Map<String, String> data, HttpSession session) {
+
 		String calc_id = data.get("id");
 		String boardTitle = data.get("title");
 		String boardContent = data.get("content");
 		List<SavecalDTO> scList = scService.findById(calc_id);
-		
+
 		double total = 0;
-		
-		for(SavecalDTO sc : scList) {
+
+		for (SavecalDTO sc : scList) {
 			int matNo = sc.getMaterialNo();
 			int matVol = sc.getMaterialVolume();
 			MaterialsDTO mat = matService.getByMatNo(matNo);
 			double co2kg = mat.getGasKg();
-			
+
 			total += co2kg * matVol;
 		}
-		
+
 		total = Math.floor(total * 100) / 100;
-		
-		
-		
+
 		MemberDTO mem = (MemberDTO) session.getAttribute("login");
 		String mem_id = mem.getMemId();
 		String entpName = mem.getEntpName();
-		
+
 		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmssSSS"); 
+		SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmssSSS");
 		String unique = sdf.format(date);
 		unique += mem_id;
-		
+
 		BoardDTO board = new BoardDTO();
 		board.setEntpName(entpName);
 		board.setBoardId(unique);
@@ -131,47 +132,84 @@ public class BoardController {
 		board.setOrderContent(boardContent);
 		board.setCalcResult(total);
 		board.setMemId(mem_id);
-		
+
 		boardService.boardWriteDo(board);
-		
+
 		// CONFIRM YN Y로 변경
 		scService.updateConfirm(calc_id);
 		// CONFIRM YN N인 것들 삭제
 		scService.deleteConfirmN();
-		
+
 		return "board/boardView";
 	}
-	
+
 	@RequestMapping("/boardViewAdmin")
 	public String boardViewAdmin(HttpSession session, Model model, BoardDTO check, String feedbackYn) {
-		
+
 		MemberDTO login = (MemberDTO) session.getAttribute("login");
-        String memId = login.getMemId();
-        
-        check.setMemId(memId);
-        check.setFeedbackYn(feedbackYn);
-        System.out.println(check);
-        
-        System.out.println(memId);
-        System.out.println(feedbackYn);
-        
-        List<BoardDTO> getBoardListByIdConfirm = boardService.getBoardListByIdConfirm(check);
-        
-        if(getBoardListByIdConfirm.size()>0) {
-        	model.addAttribute("BoardListByIdConfirm", getBoardListByIdConfirm);
-        	System.out.println(getBoardListByIdConfirm);
-        }else {
-        	model.addAttribute("noList","작성한 글이 없습니다.");
-        }
-        
-		
+		String memId = login.getMemId();
+
+		check.setMemId(memId);
+		check.setFeedbackYn(feedbackYn);
+		System.out.println(check);
+
+		System.out.println(memId);
+		System.out.println(feedbackYn);
+
+		List<BoardDTO> getBoardListByIdConfirm = boardService.getBoardListByIdConfirm(check);
+
+		if (getBoardListByIdConfirm.size() > 0) {
+			model.addAttribute("BoardListByIdConfirm", getBoardListByIdConfirm);
+			System.out.println(getBoardListByIdConfirm);
+		} else {
+			model.addAttribute("noList", "작성한 글이 없습니다.");
+		}
+
 		return "board/boardViewAdmin";
 	}
-	
-	/*
-	 * @RequestMapping("/replyWriteDo") public String replyWriteDo(@RequestBody
-	 * Map<String>)
-	 */
-	
-	
+
+	 @ResponseBody
+	 @RequestMapping("/replyWriteDo") 
+	 public ReplyDTO replyWriteDo(@RequestBody Map<String,String> data, HttpSession session) {
+		 int b_no = Integer.parseInt(data.get("no"));
+		 String b_content = data.get("content");
+		 String calc_id = data.get("id");
+		 MemberDTO mem = (MemberDTO) session.getAttribute("login");
+		 String mem_id = mem.getMemId();
+		 
+		 BoardDTO board = boardService.getBoardByNo(b_no);
+		 String board_id = board.getBoardId();
+		 
+		 List<SavecalDTO> scList = scService.findById(calc_id);
+		 
+		 double result = 0;
+		 
+		 for(SavecalDTO sc : scList) {
+			 int no = sc.getMaterialNo();
+			 int vol = sc.getMaterialVolume();
+			 MaterialsDTO mat = matService.getByMatNo(no);
+			 double gasKg = mat.getGasKg();
+			 double res = gasKg * vol;
+			 
+			 result += res;
+		 }
+		 
+		 Date date = new Date();
+		 SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmssSSS");
+		 String unique = sdf.format(date);
+		 unique += "reply" + data.get("no");
+		 
+		 ReplyDTO rep = new ReplyDTO(unique, board_id, b_content, calc_id, result);
+		
+		 // Board 피드백 여부 변경 
+		 boardService.updateFeedbackYN(b_no);
+		 // CONFIRM YN Y로 변경
+		 scService.updateConfirm(calc_id);
+		 // CONFIRM YN N인 것들 삭제
+		 scService.deleteConfirmN();
+		 
+		 return rep;
+	 }
+	 
+
 }
